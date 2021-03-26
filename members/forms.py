9824e0b9d02b6c735689fd9724a5d603ae6691e3
arman9022 <arman.hub.bd@gmail.com>
2021-03-26 14:59:57 +0600

    Edit page style

--- members/forms.py
+++ members/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 from django import forms
 
@@ -10,8 +10,25 @@ class SignUpForm (UserCreationForm):
   class Meta:
     model=User
     fields=('username','first_name','last_name','email','password1','password2')
+
   def __init__(self,*args,**kwargs):
     super(SignUpForm,self).__init__(*args,**kwargs)
     self.fields['username'].widget.attrs['class']='form-control'
     self.fields['password1'].widget.attrs['class']='form-control'
     self.fields['password2'].widget.attrs['class']='form-control'
+
+class EditProfileForm (UserChangeForm):
+  username=forms.CharField( widget=forms.TextInput(attrs={'class':'form-control'}) )
+  email=forms.EmailField( widget=forms.EmailInput(attrs={'class':'form-control'}) )
+  first_name=forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}) )
+  last_name=forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}) )
+  last_login=forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}) )
+  is_superuser=forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class':'form-control'}) )
+  is_stuff=forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class':'form-control'}) )
+  is_active=forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class':'form-control'}) )
+  date_joined=forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}) )
+
+  class Meta:
+    model=User
+    fields=('username','email','first_name','last_name','password','last_login','is_superuser','is_stuff','is_active','date_joined')
+    
